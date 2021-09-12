2eb50beb9097728dd1b31ee9be31e55dc0917d3c
Andrés Rojas <eurekacloud2019@gmail.com>
2021-09-12 16:06:30 +0200

    add encripted message

--- js/app.js
+++ js/app.js
@@ -56,6 +56,9 @@ let decrypt = () => {
   }
 };
 
+let encriptedMessage = "¡lohasconseguido!puedesvacilaratuscompaerosescribiendoporelchat:'¡soylaleche,turingflipariaconmigo!'yacontinuacionenviaunmensajecifrado,¡elquetuquieras!";
+console.log("encriptedMessage: " + encriptedMessage);
+
 // EVENTS
 decryptButtonId.addEventListener('click', handleDecryptButton);
 encryptButtonId.addEventListener('click', handleEncryptButton);
